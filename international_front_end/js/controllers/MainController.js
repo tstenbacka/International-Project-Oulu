fa833f875df95a3febdc899e6494631151b77dc3
Teemu Karjalainen <t5kate01@students.oamk.fi>
2017-02-09 14:42:11 +0200

    Changes
    Some changes to the index.html and MainController.js. Added an error
    screen telling people that they're not supposed to be there.

--- international_front_end/js/controllers/MainController.js
+++ international_front_end/js/controllers/MainController.js
@@ -1,3 +1,5 @@
 app.controller('MainController', ['$scope', function($scope) {
-    $scope.title = 'controller-test';
+    $scope.title = 'Welcome! Tervetuloa! Welkom!';
+    $scope.subtitle = 'This is a wip site for OAMK and FONTYS international co-operation project.';
+    $scope.errormessage = 'You´re not actually supposed to be here, sorry about that!';
 }]);
