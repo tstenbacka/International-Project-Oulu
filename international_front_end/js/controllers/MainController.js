d6fd8acaac1cb6196a6add1d5d67b21f9fd8b956
t5tuja02 <t5tuja02@students.oamk.fi>
2017-02-13 14:16:18 +0200

    Added stripe and Controller

--- international_front_end/js/controllers/MainController.js
+++ international_front_end/js/controllers/MainController.js
@@ -3,3 +3,10 @@ app.controller('MainController', ['$scope', function($scope) {
     $scope.subtitle = 'This is a wip site for OAMK and FONTYS international co-operation project.';
     $scope.errormessage = 'You´re not actually supposed to be here, sorry about that!';
 }]);
+
+app.controller('CardController', ['$scope', function($scope) {
+    $scope.category = 'Running';
+    $scope.title = 'Running in the 90s';
+    $scope.time = 'Tuesday at 9am';
+    $scope.distance = '800m';
+}]);
