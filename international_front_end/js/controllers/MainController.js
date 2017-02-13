5db2510afe5be13d74042f8fa42e29cc11f32e24
armageddom <jere.kylatie@hotmail.com>
2017-02-13 14:41:45 +0200

    header vielä kesken

--- international_front_end/js/controllers/MainController.js
+++ international_front_end/js/controllers/MainController.js
@@ -3,3 +3,9 @@ app.controller('MainController', ['$scope', function($scope) {
     $scope.subtitle = 'This is a wip site for OAMK and FONTYS international co-operation project.';
     $scope.errormessage = 'You´re not actually supposed to be here, sorry about that!';
 }]);
+
+header.controller("HeaderController",["$scope", function($scope){
+
+
+}]);
+
