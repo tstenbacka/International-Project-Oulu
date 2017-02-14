9681aa27cd6ee691cd895da4e4521921a13d377b
t5tuja02 <t5tuja02@students.oamk.fi>
2017-02-14 11:04:32 +0200

    Some fixes

--- international_front_end/js/controllers/MainController.js
+++ international_front_end/js/controllers/MainController.js
@@ -10,7 +10,7 @@ app.controller('CardController', ['$scope', function($scope) {
         category : 'Running';
         title : 'Running in the 90s';
         time : 'Tuesday at 9am';
-        distance : '800m';
+        distance : '800m ';
     };
     
 }]);
