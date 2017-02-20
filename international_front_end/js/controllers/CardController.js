7035d58e3fdfca08c9c5bc0798192d6ef93066e8
t5tuja02 <t5tuja02@students.oamk.fi>
2017-02-20 13:19:55 +0200

    Cards repeat
    but can't see the data yet

--- international_front_end/js/controllers/CardController.js
+++ international_front_end/js/controllers/CardController.js
@@ -1,10 +1,20 @@
 app.controller('CardController', ['$scope', function($scope) {
 
-    $scope.runnning = {
-        category : 'Running';
-        title : 'Running in the 90s';
-        time : 'Tuesday at 9am';
-        distance : '800m ';
-    };
+    $scope.activities = [
+    {
+        category : 'Running',
+        title : 'Running in the 90s',
+        time : 'Tuesday at 9am',
+        distance : '800m'
+    },
+                         
+    {
+        category : 'Jamming',
+        title : 'Jamming in the 90s',
+        time : 'Thursday at 9am',
+        distance : '800m'
+    }
+                         
+    ];
     
 }]);
