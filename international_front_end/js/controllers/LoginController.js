42b3823d7ca9511ec89ded9ca55295fe8018e728
tstenbacka <tomi.stenbacka@gmail.com>
2017-02-20 13:53:01 +0200

    Attempted to create login functionality
    (did not succeed in doing so)

--- international_front_end/js/controllers/LoginController.js
+++ international_front_end/js/controllers/LoginController.js
@@ -1,3 +1,19 @@
-app.controller('LoginController', ['$scope', function($scope) {
+app.controller('LoginController', ['$scope', '$location', function($scope, $location) {
     $scope.msg = 'login';
+    $scope.userNamePlaceholder = 'username';
+    $scope.passWordPlaceholder = 'password';
+    $scope.userName;
+    $scope.passWord;
+    $scope.logged = false;
+
+    $scope.login = function () {
+        if ($scope.userName === $scope.passWord) {
+            //$location.path('/');
+            $scope.logged = true;
+        }
+
+        else {
+            $scope.logged = false;
+        }
+    };
 }]);
