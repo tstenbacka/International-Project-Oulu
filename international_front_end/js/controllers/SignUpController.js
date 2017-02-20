6966dcc8959fd7aa19bad2d8819cc1590cea6bf3
armageddom <jere.kylatie@hotmail.com>
2017-02-20 14:14:49 +0200

    Sing up view unfinished

--- international_front_end/js/controllers/SignUpController.js
+++ international_front_end/js/controllers/SignUpController.js
@@ -1,3 +1,25 @@
 app.controller('SignUpController', ['$scope', function($scope) {
     $scope.msg = 'sign up';
+	$scope.nameHolder = 'Name';
+	$scope.ageHolder = 'Age';
+	$scope.passwordHolder = 'Password';
+	$scope.postalCodeHoler = 'PostalCode';
+	$scope.emailHolder = 'example@example.com';
+
+	$scope.name;
+	$scope.age;
+	$scope.password;
+	$scope.postalCode;
+	$scope.email;
+	$scope.check = false;
+
+	$scope.forwardStart = function (){
+		if ($scope.name != null && $scope.age != null && $scope.password != null){
+			$scope.check = true;
+		}
+		else {
+			$scope.check = false;
+		}
+	}; 	
+	
 }]);
