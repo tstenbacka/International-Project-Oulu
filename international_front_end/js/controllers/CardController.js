27cfbf8ae93c586f1364cab23152ca33804ab050
t5tuja02 <t5tuja02@students.oamk.fi>
2017-02-20 13:27:47 +0200

    Cards repeat with information

--- international_front_end/js/controllers/CardController.js
+++ international_front_end/js/controllers/CardController.js
@@ -1,20 +1,19 @@
 app.controller('CardController', ['$scope', function($scope) {
 
     $scope.activities = [
-    {
-        category : 'Running',
-        title : 'Running in the 90s',
-        time : 'Tuesday at 9am',
-        distance : '800m'
-    },
-                         
-    {
-        category : 'Jamming',
-        title : 'Jamming in the 90s',
-        time : 'Thursday at 9am',
-        distance : '800m'
-    }
-                         
+        {
+            category : 'Running',
+            title : 'Running in the 90s',
+            time : 'Tuesday at 9am',
+            distance : '800m'
+        },
+
+        {
+            category : 'Jamming',
+            title : 'Jamming in the 90s',
+            time : 'Thursday at 9am',
+            distance : '800m'
+        }                    
     ];
     
 }]);
