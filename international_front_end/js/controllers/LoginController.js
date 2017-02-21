db843175ba448a522465101dda2ac82999ca64dc
tstenbacka <tomi.stenbacka@gmail.com>
2017-02-21 09:50:45 +0200

    Login now actually works
    Login has now a placeholder authentication system which redirects user
    to the main page after succesful authentication.

--- international_front_end/js/controllers/LoginController.js
+++ international_front_end/js/controllers/LoginController.js
@@ -2,18 +2,23 @@ app.controller('LoginController', ['$scope', '$location', function($scope, $loca
     $scope.msg = 'login';
     $scope.userNamePlaceholder = 'username';
     $scope.passWordPlaceholder = 'password';
+    $scope.userNameHelpText = 'Username is required';
+    $scope.passWordHelpText = 'Password is required';
     $scope.userName;
     $scope.passWord;
     $scope.logged = false;
+    $scope.loginError;
 
     $scope.login = function () {
         if ($scope.userName === $scope.passWord) {
             //$location.path('/');
             $scope.logged = true;
+            $location.path('/').replace();
         }
 
         else {
             $scope.logged = false;
+            $scope.loginError = 'Failed to Authenticate';
         }
     };
 }]);
