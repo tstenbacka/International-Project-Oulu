c036cb41fd287d51ba1c90c510f04393877e004a
Teemu Karjalainen <t5kate01@students.oamk.fi>
2017-02-27 12:42:25 +0200

    Functionality to submit
    Changes view to main view and logs sent json-data to console.

--- international_front_end/js/controllers/NewActivityController.js
+++ international_front_end/js/controllers/NewActivityController.js
@@ -1,14 +1,12 @@
-app.controller('NewActivityController', ['$scope', function ($scope) {
+app.controller('NewActivityController', ['$scope', '$location', function ($scope, $location) {
     $scope.viewTitle = 'Create a new activity';
 
     $scope.activityHints = {
         hintCategory: 'Category:',
         hintTitle: 'Title:',
-        hintDate: 'Date:',
-        hintTime: 'Time:',
-        hintDuration: 'Duration:',
+        hintDateTime: 'Date/Time:',
         hintLocation: 'Location:',
-        hintZipCode: 'ZipCode:',
+        hintPostalCode: 'ZipCode:',
         hintDescription: 'Description:',
         hintParticipantAmount: 'Participants:',
         hintSkillLevel: 'Skill level:'
@@ -16,15 +14,61 @@ app.controller('NewActivityController', ['$scope', function ($scope) {
 
     $scope.activityInformation = {
         activityId: 0,
-        activityCategory: 'Running',
-        activityTitle: 'Title',
-        activityDate: 'Friday',
-        activityTime: '11am',
-        activityDuration: 2,
-        activityLocation: 'Ainolanpuiston kesäteatteri',
-        activityZipCode: '90100',
-        activityDescription: 'This is a description of the activity.',
-        activityParticipantAmount: 2,
-        activitySkillLevel: 'Intermediate'
+        activityCategory: '',
+        activityTitle: '',
+        activityDateTime: new Date(Date),
+        activityLocation: '',
+        activityPostalCode: '',
+        activityDescription: '',
+        activityParticipantAmount: 0,
+        activitySkillLevel: ''
     };
+
+    $scope.skillLevels = [
+        {
+            id: '00',
+            title: 'Beginner'
+        },
+        {
+            id: '01',
+            title: 'Intermediate'
+        },
+        {
+            id: '02',
+            title: 'Advanced'
+        }
+    ];
+    
+    $scope.categories = [
+        {
+            id: '00',
+            title: 'Running'
+        },
+        {
+            id: '01',
+            title: 'Music'
+        },
+        {
+            id: '02',
+            title: 'Fencing'
+        },
+        {
+            id: '03',
+            title: 'Swimming'
+        },
+        {
+            id: '04',
+            title: 'Cycling'
+        }
+    ];
+
+    $scope.submitActivityForm = function () {
+        /* while compiling form , angular created this object*/
+        var data = $scope.activityInformation;
+        /* post to server*/
+        console.log(data);
+        // $http.post("https://jsonplaceholder.typicode.com/posts/", data);
+        $location.path('/').replace();
+    }
+
 }]);
