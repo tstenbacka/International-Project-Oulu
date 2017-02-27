b029bb3f0737681cc2b1b9e9579b950dd3e96871
tstenbacka <tomi.stenbacka@gmail.com>
2017-02-27 12:24:45 +0200

    Added buttons for new user and password reset

--- international_front_end/js/controllers/LoginController.js
+++ international_front_end/js/controllers/LoginController.js
@@ -1,9 +1,13 @@
 app.controller('LoginController', ['$scope', '$location', function($scope, $location) {
-    $scope.msg = 'login';
+    $scope.notHidden = true;
+    $scope.loginMsg = 'login';
+    $scope.newUserMsg = 'New User';
     $scope.userNamePlaceholder = 'username';
     $scope.passWordPlaceholder = 'password';
     $scope.userNameHelpText = 'Username is required';
     $scope.passWordHelpText = 'Password is required';
+    $scope.newUserButtonMsg = 'Create a new account';
+    $scope.forgotPassWordMsg = 'Forgot password?';
     $scope.userName;
     $scope.passWord;
     $scope.logged = false;
@@ -11,7 +15,6 @@ app.controller('LoginController', ['$scope', '$location', function($scope, $loca
 
     $scope.login = function () {
         if ($scope.userName === $scope.passWord) {
-            //$location.path('/');
             $scope.logged = true;
             $location.path('/').replace();
         }
@@ -20,5 +23,10 @@ app.controller('LoginController', ['$scope', '$location', function($scope, $loca
             $scope.logged = false;
             $scope.loginError = 'Failed to Authenticate';
         }
+    };
+
+    $scope.signUp = function () {
+        $scope.notHidden = false;
+        $location.path('/signup').replace();
     };
 }]);
