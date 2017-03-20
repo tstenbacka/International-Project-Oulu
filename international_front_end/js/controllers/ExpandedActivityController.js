5efc53c00bfa565c7862806fb06f5dd67bf960ef
t5tuja02 <t5tuja02@students.oamk.fi>
2017-03-20 09:42:50 +0200

    Expanded Activity now gets most of the data from backend

--- international_front_end/js/controllers/ExpandedActivityController.js
+++ international_front_end/js/controllers/ExpandedActivityController.js
@@ -1,3 +1,10 @@
-app.controller('ExpandedActivityController', ['$scope', function($scope) {
-
+app.controller('ExpandedActivityController', ['$scope','$http', function($scope, $http) {
+    $scope.test = "Test";
+    $http.get('http://192.81.223.10:8080/Oulu_Backend/webapi/activities/1').then(function(response) {
+        $scope.activity = response.data;
+        console.log(response.data);
+    }, function(response){
+        $scope.activities = "Something went wrong";
+        console.log("WRONG");
+    });
 }]);
