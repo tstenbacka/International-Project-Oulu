55fbae174d0ef50a5c299834d8ea5d03469fcbee
tstenbacka <tomi.stenbacka@gmail.com>
2017-03-30 09:44:40 +0300

    crappy code that does stuff

--- international_front_end/js/controllers/MainController.js
+++ international_front_end/js/controllers/MainController.js
@@ -2,4 +2,24 @@ app.controller('MainController', ['$scope', function($scope) {
     $scope.signInTxt = 'Sign In';
     $scope.newActivityTxt = 'New Activity';
     $scope.searchTxt = "I Want To ...";
+    $scope.loggedIn = document.cookie;
+
+    $scope.loggedIn = function () {
+        if(document.cookie.length > 0)
+            return true;
+        else
+            return false;
+    }
+
+    $scope.getDudesName = function () {
+        if(document.cookie.length > 0) {
+            var userObject = JSON.parse(document.cookie);
+           // $scope.searchTxt = userObject.username;
+            return userObject.username;
+        }
+
+        else {
+            return 'Sign In';
+        }
+    }
 }]);
