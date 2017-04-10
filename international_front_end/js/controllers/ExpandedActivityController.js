606530d2c681526ebbfa870dd28ebfd82711e3b2
t5tuja02 <t5tuja02@students.oamk.fi>
2017-04-10 09:39:19 +0300

    Base for joining activity

--- international_front_end/js/controllers/ExpandedActivityController.js
+++ international_front_end/js/controllers/ExpandedActivityController.js
@@ -6,4 +6,33 @@ app.controller('ExpandedActivityController', ['$scope','$http','$routeParams', f
     }, function(response){
         console.log("Couldn't fetch data");
     });
+    
+    var userObject = JSON.parse(document.cookie);
+
+    $scope.userId = {
+        id: userObject.id,
+    };
+  
+    var data = $scope.userId;
+    var url = "http://192.81.223.10:8080/Oulu_Backend/webapi/activities"
+    console.log(JSON.stringify(data));
+    //var userObject = JSON.parse(document.cookie);
+    // var y = 'Bearer ' + userObject.token;
+    //console.log(y);
+    $http.defaults.headers.post.Authorization = 'Bearer ' + userObject.token;
+
+    $http.post(url, data)
+        .then(
+            function (response) {
+                // success callback
+                console.log("SUCCESS");
+                //console.log(response);
+
+            },
+            function (response) {
+                // failure callback
+                console.log("FAILURE");
+                //console.log(response);
+            }
+        );
 }]);
