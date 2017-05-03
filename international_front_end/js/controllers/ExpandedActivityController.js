5a82299923c90164e115295d4b9b587ee740e8b6
armageddom <jere.kylatie@hotmail.com>
2017-05-03 13:57:52 +0300

    ei tulosta addressiä

--- international_front_end/js/controllers/ExpandedActivityController.js
+++ international_front_end/js/controllers/ExpandedActivityController.js
@@ -7,7 +7,20 @@ app.controller('ExpandedActivityController', ['$scope', '$http', '$routeParams',
         $http.get('http://192.81.223.10:8080/Oulu_Backend/webapi/activities/').then(function (response) {
             $scope.activity = response.data[$routeParams.id];
             $scope.activityId = $routeParams.id;
-
+                  
+            var jsonString = JSON.stringify($scope.activity);    
+            var jsonValue = JSON.parse(jsonString);  
+            var X = jsonValue["locationX"];
+            var Y = jsonValue["locationY"];
+            var address;
+            
+            
+    
+            
+            
+            
+            
+            
 
             // Parse userID out of cookie
             var userObject = JSON.parse(document.cookie);
@@ -15,11 +28,42 @@ app.controller('ExpandedActivityController', ['$scope', '$http', '$routeParams',
             $scope.user = {
                 id: userObject.id
             };
-
+            
+            
+            
 
             // Save activityID and userID for later use
             activityId = $scope.activityId;
             userId = $scope.user.id;
+            
+                        
+            function geocodeLatLng(geocoder) {
+                var input = Y + ',' + X;
+                console.log(input);
+                var latlngStr = input.split(',', 2);
+                var latlng = {lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1])};
+                var geocoder = new google.maps.Geocoder();
+                geocoder.geocode({ 'latLng': latlng }, function (results, status) {
+                    if (status !== google.maps.GeocoderStatus.OK) {
+                        alert("Activity location is not defined");
+                    }
+                    // This is checking to see if the Geoeode Status is OK before proceeding
+                    if (status == google.maps.GeocoderStatus.OK) {
+                        console.log(results);
+                        address = (results[0].formatted_address);
+                        //console.log(address);
+                        
+                    }
+                });
+            }
+
+                
+            geocodeLatLng();            
+            
+
+            
+            console.log(address);
+            //console.log(Y);
 
         }, function (response) {
             console.log("Couldn't fetch data for expanded activity.");
@@ -58,4 +102,6 @@ app.controller('ExpandedActivityController', ['$scope', '$http', '$routeParams',
             }
         );
     }
+    
+
 }]);
