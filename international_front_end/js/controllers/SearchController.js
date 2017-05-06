b306073fcd01a7761e206c61fe73c77a7c11bdac
jturunen <jannesanteriturunen@live.com>
2017-05-06 11:48:33 +0300

    Fixed link for search by keyword
    Doesnt show any activities, bug.

--- international_front_end/js/controllers/SearchController.js
+++ international_front_end/js/controllers/SearchController.js
@@ -3,10 +3,13 @@ app.controller('SearchController', ['$scope','$http','sharedProperties','$timeou
    
     var y = JSON.parse(document.cookie);
     $scope.searchword = y.search;
+    $scope.longitude = y.homeLong;
+    $scope.latitude = y.homeLat;
    // $scope.searchword = sharedProperties.getProperty();
 
-    var search_url = "http://192.81.223.10:8080/Oulu_Backend/webapi/activities/byKey/" + $scope.searchword;
-    
+    //var search_url = "http://192.81.223.10:8080/Oulu_Backend/webapi/activities/byKey/" + $scope.searchword;
+    var search_url = "http://192.81.223.10:8080/Oulu_Backend/webapi/activities/byKey/" + $scope.searchword + "?lon=" + $scope.longitude + "&lat=" +  $scope.latitude;
+    console.log(search_url);
     //Open activity in expanded activity view by id we get from card.
     $scope.openActivity =  function(id) {
         activityIdService.setProperty(id)
