cb375ba2c28f14e75fe273ab5b61b97e68344895
tstenbacka <tomi.stenbacka@gmail.com>
2017-05-09 11:44:42 +0300

    different search when logged in

--- international_front_end/js/controllers/SearchController.js
+++ international_front_end/js/controllers/SearchController.js
@@ -1,5 +1,25 @@
 app.controller('SearchController', ['$scope','$http','sharedProperties','$timeout','activityIdService',function($scope, $http, sharedProperties,$timeout,activityIdService) {
     
+
+        if(document.cookie.length > 0) {
+        try {
+        var userObject = JSON.parse(document.cookie);
+        }
+        catch (err) {
+            
+        }
+        $scope.searchword = userObject.search;
+        if(userObject.username) {
+            $scope.longitude = userObject.homeLong;
+            $scope.latitude = userObject.homeLat
+        }
+        else {
+        }
+    }
+
+    else {
+        
+    }
    
     var y = JSON.parse(document.cookie);
     $scope.searchword = y.search;
@@ -7,18 +27,20 @@ app.controller('SearchController', ['$scope','$http','sharedProperties','$timeou
     $scope.latitude = y.homeLat;
    // $scope.searchword = sharedProperties.getProperty();
 
-    //var search_url = "http://192.81.223.10:8080/Oulu_Backend/webapi/activities/byKey/" + $scope.searchword;
-    var search_url = "http://192.81.223.10:8080/Oulu_Backend/webapi/activities/byKey/" + $scope.searchword + "?lon=" + $scope.longitude + "&lat=" +  $scope.latitude;
-    console.log(search_url);
-    //Open activity in expanded activity view by id we get from card.
-    $scope.openActivity =  function(id) {
-        activityIdService.setProperty(id)
-        window.location="#/activity/"+id;
+    function url_check (){
+   
+        var search_url = "http://192.81.223.10:8080/Oulu_Backend/webapi/activities/byKey/" + $scope.searchword;
+        var search_url_lat = "http://192.81.223.10:8080/Oulu_Backend/webapi/activities/byKey/" + $scope.searchword + "?lon=" + $scope.longitude + "&lat=" +  $scope.latitude;
+
+        if(userObject.username)
+            return search_url_lat;
+        else
+            return search_url;
     }
         
 
     $scope.pageInfo = 'Choose a card';  
-       $http.get(search_url).then(function(response) {
+       $http.get(url_check()).then(function(response) {
         $scope.activities = response.data;
         console.log(response.data);
     }, function(response){
